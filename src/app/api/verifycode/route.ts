b53423d7701fed2ddc8dfc3cde39764e9ebe2b5f
garvitvarshney-16 <garvitvarshney81@gmail.com>
2024-07-04 00:29:01 +0530

    ok

--- src/app/api/verifycode/route.ts
+++ src/app/api/verifycode/route.ts
@@ -4,5 +4,71 @@ import UserModel from "@/model/User";
 import { z } from "zod";
 
 export async function GET(request: Request) {
-    
+
+}
+
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    try {
+
+        const { username, code } = await request.json()
+
+        const decodedUsername = decodeURIComponent(username)
+        const user = await UserModel.findOne({ username: decodedUsername })
+
+        if (!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                },
+                { status: 500 }
+            )
+        }
+
+        const isValidCode = user.verifyCode === code
+
+        const isExpiredNotCode = new Date(user.verifyCodeExpiry) > new Date()
+
+        if (isValidCode && isExpiredNotCode) {
+            user.isVerified = true
+            await user.save()
+        } else if (!isExpiredNotCode) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Verification code has expired, please signup again to get a new code"
+                },
+                { status: 400 }
+            )
+        } else {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Incorrect verification code"
+                },
+                { status: 400 }
+            )
+        }
+
+        return Response.json(
+            {
+                success: true,
+                message: "Account verified successfully"
+            },
+            { status: 200 }
+        )
+
+    } catch (error) {
+        console.log("Error verifying user", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Error verifying user"
+            },
+            { status: 500 }
+        )
+    }
 }
