c6674149ac24b4aebe5fb735f27aa9511fb2b638
garvitvarshney-16 <garvitvarshney81@gmail.com>
2024-07-21 02:08:39 +0530

    ok

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,19 +1,123 @@
 'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
+import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import Link from 'next/link'
+import { useDebounceValue } from 'usehooks-ts'
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from 'next/navigation'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { signUpSchema } from '@/schemas/signUpSchema'
+import axios, { AxiosError } from 'axios'
+import { ApiResponse } from '@/types/ApiResponse'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from "@/components/ui/input"
+
+
+
+const page = () => {
+
+  const [username, setUsername] = useState('');
+  const [usernameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const debouncedUsername = useDebounceValue(username, 300);
+  const { toast } = useToast()
+  const router = useRouter()
+
+  // zod implementation
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    }
+  })
+
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true)
+        setUsernameMessage('')
+        try {
+          const response = await axios.get(`/api/check-username-unique?username=${debouncedUsername}`)
+          setUsernameMessage(response.data.message);
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(
+            axiosError.response?.data.message ?? "Error checking username"
+          )
+        } finally {
+          setIsCheckingUsername(false)
+        }
+      }
+    }
+    checkUsernameUnique()
+  }, [debouncedUsername])
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true);
+    try {
+      const res = await axios.post<ApiResponse>('/api/sign-up', data)
+      toast({
+        title: 'Success',
+        description: res.data.message
+      })
+      router.replace(`/verify/${username}`)
+      setIsSubmitting(false);
+    } catch (error) {
+      console.log("Error in signup of user", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+      let errorMessage = axiosError.response?.data.message
+      toast({
+        title: "Signup failed",
+        description: errorMessage,
+        variant: "destructive"
+      })
+      setIsSubmitting(false)
+    }
   }
+
+
+
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-orange-500 px-3 py-1 m-4 rounded" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div className='flex justify-center items-center min-h-screen bg-gray-100'>
+      <div className='w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md'>
+        <div className='text-center'>
+          <h1 className='text-4xl font-extrabold tracking-tight lg:text-5xl mb-6'>
+            Join Mystery Message
+          </h1>
+          <p className='mb-4'>Sign up to start your anonymous adventure</p>
+        </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="username" {...field}
+                    onChange={(e) => {
+                      field.onChange(e)
+                      setUsername(e.target.value);
+                    }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+
+        </Form>
+      </div>
+    </div>
   )
 }
+
+export default page
--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,5 +1,8 @@
 import dbConnect from "@/lib/dbConnect";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextResponse } from 'next/server';
 
 const API = process.env.GEMINI_KEY || "";
 const genAI = new GoogleGenerativeAI(API);
@@ -9,7 +12,8 @@ export async function POST(request: Request) {
 
     try {
         const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-        const { prompt } = await request.json();
+        // const { prompt } = await request.json();
+        const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started?||If you could have dinner with any historical figure, who would it be?||What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
         const result = await model.generateContentStream(prompt);
         const response = await result.response;
@@ -25,3 +29,43 @@ export async function POST(request: Request) {
         return new Response(JSON.stringify({ success: false, message: error }), { status: 500 });
     }
 }
+
+
+
+
+
+
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// export const runtime = 'edge';
+
+// export async function POST(req: Request) {
+//   try {
+//     const prompt =
+//       "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+//     const response = await openai.completions.create({
+//       model: 'gpt-3.5-turbo-instruct',
+//       max_tokens: 400,
+//       stream: true,
+//       prompt,
+//     });
+
+//     const stream = OpenAIStream(response);
+    
+    
+//     return new StreamingTextResponse(stream);
+//   } catch (error) {
+//     if (error instanceof OpenAI.APIError) {
+//       // OpenAI API error handling
+//       const { name, status, headers, message } = error;
+//       return NextResponse.json({ name, status, headers, message }, { status });
+//     } else {
+//       // General error handling
+//       console.error('An unexpected error occurred:', error);
+//       throw error;
+//     }
+//   }
+// }
